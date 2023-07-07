b91702fe86eb5c7e1876fad95c9f2b4741c8fd64
Sacramentix <benji.u1225u@gmail.com>
2023-07-07 21:39:12 +0200

    add zod with working type infer

--- packages/server/src/routes/updatePixel.ts
+++ packages/server/src/routes/updatePixel.ts
@@ -1,21 +1,25 @@
-import { Response, URLPattern } from 'fets';
 import { router } from '../lib/server.js';
 import { canvas } from '../lib/canvas.js';
 import { wss } from '../index.js';
+import { z } from 'zod';
 
 const UPDATE_PIXEL_PATH = '/update/pixel/:x/:y/:color';
 
 router.route({
   path: UPDATE_PIXEL_PATH,
   method: 'GET',
-  handler: async (req: Request) => {
-    const pattern = new URLPattern({ pathname: UPDATE_PIXEL_PATH });
-    const { x, y, color } = pattern.exec(req.url)?.pathname.groups as any;
-    const xint = parseInt(x);
-    const yint = parseInt(y);
-    if (canvas[yint]![xint] != null) {
-      canvas[yint]![xint] = "#" + color;
+  schemas: {
+    request: {
+      params: z.object({
+        x: z.number().int().min(0).max(4),
+        y: z.number().int().min(0).max(4),
+        color: z.string().regex(/^#[0-9A-F]{6}$/)
+      })
     }
+  },
+  handler: async (req) => {
+    const { x, y, color } = req.params;
+    canvas[y]![x] = "#" + color;
     wss.clients.forEach(client => {
       if (client.readyState === 1) {
         client.send(`${x}/${y}/${"#" + color}`);
